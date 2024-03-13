e88dd1a8723fee2fcfbf9c216e35e2c8f2a9ff45
AkashPak <akashp8@nycstudents.net>
2024-03-13 09:21:08 -0400

    dsa

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,10 +3,39 @@
 </template>
 
 <script>
-import BarChart from 'views/to/component/BarChart'
+import BarChart from 'path/to/component/BarChart'
 
 export default {
   name: 'App',
   components: { BarChart }
+}
+</script><template>
+  <Bar
+    id="my-chart-id"
+    :options="chartOptions"
+    :data="chartData"
+  />
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March' ],
+        datasets: [ { data: [40, 20, 12] } ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
 }
 </script>
