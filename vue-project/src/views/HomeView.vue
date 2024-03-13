59f78701176ca33d4534fda2c79d83ab9575d007
markizzyv <144455870+markizzyv@users.noreply.github.com>
2024-03-13 09:19:53 -0400

    s
    s

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,11 +1,33 @@
 <template>
+
   <div>
 
+
+
   </div>
+
 </template>
 
+
+
 <script>
- 
+
+// https://data.cityofnewyork.us/resource/uip8-fykc.json
+import { ref, onMounted } from 'vue';
+let apiURL = ref('');
+async function getData() {
+  let res = await fetch("https://data.cityofnewyork.us/resource/s67q-ee5u.json")
+  let data = await res.json();
+  console.log(data)
+  apiURL.value = data;
+}
+
+onMounted (() => {
+  getData();
+})
+
+
+
 </script>
 
 <style scoped>
