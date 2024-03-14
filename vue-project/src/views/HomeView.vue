465cc16abcabec047b62071b11f670932d541a7e
AkashPak <akashp8@nycstudents.net>
2024-03-14 08:57:57 -0400

    sad

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -12,11 +12,11 @@
 
 <script>
 
-// https://data.cityofnewyork.us/resource/uip8-fykc.json
+const api= "https://data.cityofnewyork.us/resource/uip8-fykc.json"
 import { ref, onMounted } from 'vue';
 let apiURL = ref('');
 async function getData() {
-  let res = await fetch("https://data.cityofnewyork.us/resource/s67q-ee5u.json")
+  let res = await fetch(api)
   let data = await res.json();
   console.log(data)
   apiURL.value = data;
