6210116369267d53a55ea4876475d568e897e3b0
markizzyv <144455870+markizzyv@users.noreply.github.com>
2024-03-20 09:23:00 -0400

    g
    k

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,16 +1,80 @@
-
 <template>
-<BarChart 
-/>
-</template>
 
-<script>
-import BarChart from './components/icons/BarChart.vue';
-<template>
-  <BarChart ref="bar" />
-</template>
-export default {
-  name: 'App',
-  components: { BarChart }
-}
-</script>
+<header>
+    <div>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+
+
+
+  <BarChart 
+  
+  />
+  </template>
+  
+  <script>
+  import BarChart from './components/icons/BarChart.vue';
+  
+  export default {
+    name: 'App',
+    components: { BarChart }
+  }
+  </script>
+  
+  <style scoped>
+
+  nav {
+    width: 100%;
+    font-size: 12px;
+    text-align: center;
+    margin-top: 2rem;
+  }
+  
+  nav a.router-link-exact-active {
+    color: var(--color-text);
+  }
+  
+  nav a.router-link-exact-active:hover {
+    background-color: transparent;
+  }
+  
+  nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+  }
+  
+  nav a:first-of-type {
+    border: 0;
+  }
+  
+  @media (min-width: 1024px) {
+    header {
+      display: flex;
+      place-items: center;
+      padding-right: calc(var(--section-gap) / 2);
+    }
+  
+   
+  
+    nav {
+      text-align: left;
+      margin-left: -1rem;
+      font-size: 1rem;
+  
+      padding: 1rem 0;
+      margin-top: 1rem;
+    }
+  }
+  
+  
+  </style>
+  
+
