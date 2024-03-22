bd2d044693517b8ab8c545e29f605414da10c143
AkashPak <akashp8@nycstudents.net>
2024-03-22 09:26:31 -0400

    sadw

--- vite-project/src/components/icons/BarChart.vue
+++ vite-project/src/components/icons/BarChart.vue
@@ -6,10 +6,13 @@
     />
   </template>
   
-  <script>
+  <script async>
   import { Bar } from 'vue-chartjs'
   import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
   
+
+
+
   ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
   
   export default {
@@ -28,3 +31,17 @@
     }
   }
   </script>
+
+  <script setup>
+  const api= "https://data.cityofnewyork.us/resource/uip8-fykc.json"
+import { ref, onMounted } from 'vue';
+let apiURL = ref('');
+async function getData() {
+  let res = await fetch(api)
+  let data = await res.json();
+  console.log(data)
+  apiURL.value = data;
+};
+const data = onMounted (() => {
+  getData();
+}) </script>
