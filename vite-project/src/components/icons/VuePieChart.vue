17bfba59b6e1ff0951b22b2c7fa8f6dd9f37c8cb
markizzyv <144455870+markizzyv@users.noreply.github.com>
2024-03-27 08:47:39 -0400

    n
    f

--- vite-project/src/components/icons/VuePieChart.vue
+++ vite-project/src/components/icons/VuePieChart.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <div class="container mt-5">
         <h2 class="mb-4 text-center">Vue 3 Vuechart Js 5 Pie Chart Demo</h2>
         <Pie id="my-chart-id" :options="chartOptions" :data="chartData" />
@@ -30,4 +30,4 @@ export default {
         }
     }
 }
-</script>
+</script> -->
