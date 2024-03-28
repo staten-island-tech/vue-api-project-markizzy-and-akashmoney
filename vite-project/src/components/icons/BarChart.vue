9702c030f5f3060339639aa5e21f39414d860a74
markizzyv <144455870+markizzyv@users.noreply.github.com>
2024-03-28 09:23:55 -0400

    cd
    c

--- vite-project/src/components/icons/BarChart.vue
+++ vite-project/src/components/icons/BarChart.vue
@@ -1,37 +1,43 @@
 <template>
-    <Bar
-      id="my-chart-id"
-      :options="chartOptions"
-      :data="chartData"
-    />
-  </template>
-  
-  <script>
-  import { Bar } from 'vue-chartjs'
-  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-  
-  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-  
-  export default {
-    name: 'BarChart',
-    components: { Bar },
-    data() {
-      return {
-        chartData: {
-          labels: [ 'January', 'February', 'March', 'April' ],
-          datasets: [ { data: [40, 20, 12, 2] } ]
-        },
-        chartOptions: {
-          responsive: true
-        }
-      }
+  <div class="container">
+    <Bar id="my-chart-id"
+ :options="chartOptions"
+ v-if="loaded" 
+ :data="chartData" /> />
+  </div>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async mounted () {
+    this.loaded = false
+
+    try {
+      const { userlist } = await fetch('/api/userlist')
+      this.chartdata = userlist
+
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
     }
   }
-  </script>
-
+}
+</script>
+ 
 <script setup>
 
-const api = "https://data.cityofnewyork.us/resource/uip8-fykc.json"
+const api = "https://data.cityofnewyork.us/resource/uip8-fykc.json";
 
 import {ref , onMounted } from 'vue';
 let apiUrl = ref('')
