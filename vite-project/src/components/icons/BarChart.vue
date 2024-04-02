8d1c7de9bbc9ac034ecfe097699473a5d21ce904
markizzyv <144455870+markizzyv@users.noreply.github.com>
2024-04-01 20:05:21 -0400

    s
    s

--- vite-project/src/components/icons/BarChart.vue
+++ vite-project/src/components/icons/BarChart.vue
@@ -1,45 +1,33 @@
 <template>
-    <Bar
-      id="my-chart-id"
-      :options="chartOptions"
-      :data="chartData"
-    />
-  </template>
-  
-  <script>
-  import { Bar } from 'vue-chartjs'
-  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-  
-  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-  
-  export default {
-    name: 'BarChart',
-    components: { Bar },
-    data() {
-      return {
-        chartData: {
-          labels: [ 'January', 'February', 'March' ],
-          datasets: [ { data: [40, 20, 12] } ]
-        },
-        chartOptions: {
-          responsive: true
-        }
+  <Bar
+    id="my-chart-id"
+    :options="chartOptions"
+    :data="chartData"
+    class="chart-container" 
+  />
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: ['January', 'February', 'March'],
+        datasets: [{ data: [40, 20, 12] }]
+      },
+      chartOptions: {
+        responsive: true
       }
     }
   }
-  </script>
+}
+</script>
 
-<script setup>
-const api = "https://data.cityofnewyork.us/resource/uip8-fykc.json"
+<style src="./styles.css"></style> <!-- Import external stylesheet -->
 
-import {ref , onMounted } from 'vue';
-let apiUrl = ref('')
-async function getData (){
-  let res = await fetch(api)
-  let data = await res.json();
-  console.log(data)
-  apiUrl.value = data;
-};
-const data = onMounted (()=> {
-  getData();
-})</script>
+
+
