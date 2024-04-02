b029b54ad2b6f61fc786cb3cdfe2ca6e93610823
markizzyv <144455870+markizzyv@users.noreply.github.com>
2024-04-02 08:47:21 -0400

    m
    b

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -3,6 +3,15 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 <template>
+ <header>
+    <div>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+    </header>
   <BarChart />
 </template>
 
--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -3,16 +3,18 @@ import TheWelcome from '../components/TheWelcome.vue';
 import BarChart from '@/components/icons/BarChart.vue'
 </script>
 
+
+
+
 <template>
-  <div class="container">
-    <h1 class="title">Hello, Vue!</h1>
+  <div class = "page">
+    <h1>Arrest Data</h1>
   </div>
 </template>
 
-<script>
-import './style.css'; // Import external stylesheet
 
-export default {
-  // Component logic
-};
-</script>
+<style>
+.h1{
+  color:red
+}
+</style>
